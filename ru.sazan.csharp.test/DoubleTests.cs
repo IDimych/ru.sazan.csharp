3a4d17a0a2b27f4ace1f9f01c124dc5af40f11e7
IDimych <help-document@yandex.ru>
2016-10-30 23:22:16 +0300

    Tutorial 2

---
+++ ru.sazan.csharp.test/DoubleTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ru.sazan.csharp.test
+{
+    [TestClass]
+    public class DoubleTests
+    {
+        public double Price;
+
+        [TestMethod]
+        public void default_Price_value_test()
+        {
+            Assert.AreEqual(0, Price);
+        }
+
+        [TestMethod]
+        public void change_Price_value_test()
+        {
+            Price = 55.01;
+            Assert.AreEqual(55.01, Price);
+        }
+    }
+}
